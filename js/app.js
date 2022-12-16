becdc68748b25dcf834f42f972a4494a2f46d6a6
Nanda Nippani <nandanippani@yahoo.com>
2022-12-16 15:02:52 +0000

    Added gitgnore

--- js/app.js
+++ js/app.js
@@ -3,27 +3,33 @@
 // grab data related to user's search
 //Inject the movie items into DOM based  on user's search
 
-var searchInput = document.querySelector('.search');
-var itemWrapper = document.querySelector('main');
+var searchInput = $('.search');
+var itemWrapper = $('main');
 
 function displayMatches(matches) {
-    itemWrapper.innerHTML = '';
-    for (var matchobj of matches) {
-        itemWrapper.insertAdjacentHTML('beforeend', `
+    itemWrapper.html('');
+    if (!matches) {
+        itemWrapper.html('<p class="no-search">No Results Found</p>')
+    } else {
+        for (var matchobj of matches) {
+            itemWrapper.insertAdjacentHTML('beforeend', `
     <div class="movie-item" style= "background-image: linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url(${matchobj.image_url})">
     <h3>${matchobj.title}</h3>
     <p>${matchobj.description}</p>
     <a href="${matchobj.imdb_url}"target= "_blank">View More Details</a></div>`)
+        }
     }
-
 }
 
 
 
 function getMovieData(event) {
     var keyCode = event.keyCode;
-    var searchText = searchInput.value.trim().toLowerCase();
+    var searchText = searchInput.val().trim();
     if (keyCode === 13 && searchText) {
+var responsePromise
+
+
         var matches = [];
         for (var movie of movieData) {
             if (movie.title.toLowerCase().includes(searchText)) {
@@ -32,8 +38,8 @@ function getMovieData(event) {
             }
         }
 
-       var responsePromise = fetch ('https://www.omdbapi.com/?apikey=56c29f25&s=${searchText}')
-        then 
+        var responsePromise = fetch('https://www.omdbapi.com/?apikey=56c29f25&s=${searchText}')
+        then
         displayMatches(matches);
     }
 
